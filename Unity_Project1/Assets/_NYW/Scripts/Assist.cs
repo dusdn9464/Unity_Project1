4fe1af6efb16234bebb8dc2ea4e9eb7ceeaec031
남연우 <yeonu9464@gmail.com>
2020-06-02 14:23:43 +0900

    총알클래스 생성
    -총알프리팹만들기
    -플레이어 총알발사
    -플레이어 꼬리잇기
    - 플레이어 보조비행기 생성
    -보조비행기 자동 총알발사

---
+++ Unity_Project1/Assets/_NYW/Scripts/Assist.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Assist : MonoBehaviour
+{
+    public GameObject left;
+    public GameObject right;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            left.SetActive(true);
+            right.SetActive(true);
+        }
+    }
+}
---
+++ Unity_Project1/Assets/_NYW/Scripts/AutoBullet.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoBullet : MonoBehaviour
+{
+    public GameObject bulletFactory;        //총알 프리팹
+    public GameObject firePoint;            //총알 발사위치
+
+    bool isStart = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isStart)
+        {
+            StartCoroutine("Fire");
+        }
+
+    }
+
+    IEnumerator Fire()
+    {
+        isStart = false;
+        GameObject bullet = Instantiate(bulletFactory);
+        bullet.transform.position = firePoint.transform.position;
+
+        yield return new WaitForSeconds(0.2f);
+        isStart = true;
+    }
+
+ 
+}
