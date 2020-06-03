fdc371b909059d03b3f690f7d9070f7bb79378f4
남연우 <yeonu9464@gmail.com>
2020-06-03 12:15:41 +0900

    -DestroyZone 클래스 생성 -Layer 생성

---
+++ Unity_Project1/Assets/_NYW/Scripts/DestroyZone.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyZone : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        //이곳에서 트리거에 감지된 오브젝트 제거하기(총알, 에너미)
+        Destroy(other.gameObject);
+    }
+}
