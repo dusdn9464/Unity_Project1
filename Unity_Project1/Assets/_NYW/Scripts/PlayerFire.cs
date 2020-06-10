61cb69222b5deb456c65ebacc1b787e8e078cd3a
남연우 <yeonu9464@gmail.com>
2020-06-10 17:40:50 +0900

    2020/06/10
    -스타트씬
    -버튼2개추가
    -레이저 이미지 추가

--- Unity_Project1/Assets/_NYW/Scripts/PlayerFire.cs
+++ Unity_Project1/Assets/_NYW/Scripts/PlayerFire.cs
@@ -81,7 +81,7 @@ public class PlayerFire : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Fire();
+        //Fire();
         //FireRay();
         //레이져 보여주는 기능이 활성화 되어 있을때만
         //레이져를 보여준다
@@ -224,11 +224,74 @@ public class PlayerFire : MonoBehaviour
     public void OnFireButtonClick()
     {
         //총알 게임오브젝트 생성
-        GameObject bullet = Instantiate(bulletFactory);
-        //총알 오브젝트의 위치 지정
-        //bullet.transform.position = transform.position;
-        bullet.transform.position = firePoint.transform.position;
+        //GameObject bullet = Instantiate(bulletFactory);
+        ////총알 오브젝트의 위치 지정
+        ////bullet.transform.position = transform.position;
+        //bullet.transform.position = firePoint.transform.position;
+        //
+        //SceneMgr.Instance.LoadScene("StartScene");
 
-        SceneMgr.Instance.LoadScene("StartScene");
+        //4. 큐 오브젝트풀링 사용하기
+        if (bulletPool.Count > 0)
+        {
+            GameObject bullet = bulletPool.Dequeue();
+            bullet.SetActive(true);
+            bullet.transform.position = firePoint.transform.position;
+            bullet.transform.up = firePoint.transform.up;
+        }
+        else
+        {
+            //총알 오브젝트 생성한다
+            GameObject bullet = Instantiate(bulletFactory);
+            bullet.SetActive(false);
+            //생성된 총알 오브젝트를 풀에 담는다
+            bulletPool.Enqueue(bullet);
+        }
+    }
+
+    public void OnLaserButtonClick()
+    {
+        //레이져 사운드 재생
+        audio.Play();
+
+        //라인렌더러 컴포넌트 활성화
+        lr.enabled = true;
+        //라인 시작점, 끝점
+        lr.SetPosition(0, transform.position);
+        //lr.SetPosition(1, transform.position + Vector3.up * 10);
+        //라인의 끝점은 충돌된 지점으로 변경한다
+
+        //Ray로 충돌처리
+        Ray ray = new Ray(transform.position, Vector3.up);
+        RaycastHit hitInfo; //Ray와 충돌된 오브젝트의 정보를 담는다
+                            //Ray랑 충돌된 오브젝트가 있다
+        if (Physics.Raycast(ray, out hitInfo))
+        {
+            //레이져의 끝점 지정
+            lr.SetPosition(1, hitInfo.point);
+            //충돌된 오브젝트 모두 지우기
+            //Destroy(hitInfo.collider.gameObject);
+
+            //디스트로이존의 탑과는 충돌처리 되지 않도록 한다
+            if (hitInfo.collider.name != "Top")
+            {
+                Destroy(hitInfo.collider.gameObject);
+            }
+
+            //충돌된 에너미 오브젝트 삭제
+            //프리팹으로 만든 오브젝트 같은경우는 생성될때 클론으로 생성된다
+            //Contains("Enemy") => Enemy(clone) 이런것도 포함함
+            //if (hitInfo.collider.name.Contains("Enemy"))
+            //{
+            //    Destroy(hitInfo.collider.gameObject);
+            //}
+
+        }
+        else
+        {
+            //충돌된 오브젝트가 없으니 끝점을 정해준다
+            lr.SetPosition(1, transform.position + Vector3.up * 10);
+        }
+
     }
 }
